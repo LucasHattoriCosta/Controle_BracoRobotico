57f9b57e374949bef35f9a17673814d93ed20d0d
lucas-hattori-costa <60410592+lucas-hattori-costa@users.noreply.github.com>
2020-06-27 19:55:13 -0300

    Update ITAE.m

--- ITAE.m
+++ ITAE.m
@@ -5,55 +5,16 @@ clear; close all; clc;
 %%% Deixa os eixos em LaTeX
 set(groot, 'defaultTextInterpreter','latex');
 
-%% Definindo FT's
-
-%%% Parametros
-m = 2;
-Mbase = 6;
-L = 0.5;
-c = 8.5e-5;
-b = 7.12e-3;
-g = 9.81;
-
-%%% Espaco de estados
-
-M = [1, 0, 0, 0, 0, 0; 0, (2*m+Mbase)*L, 0, 3*m*(L^2)/2, 0, m*(L^2)/2; 0, 0, 1, 0, 0, 0; 0, 2*m*L, 0, 3*m*(L^2)/2, 0, 2*m*(L^2)/3; 0, 0, 0, 0, 1, 0; 0, m*L/2, 0, m*(L^2)/6, 0, m*(L^2)/3];
-
-I = eye(6);
-
-Minv = I/M;
-
-Atil = [0, 1, 0, 0, 0, 0; 0, -b*L, 0, 0, 0, 0; 0, 0, 0, 1, 0, 0; 0, 0, -3*m*L*g/2, -c, -m*L*g/2, 0; 0, 0, 0, 0, 0, 1; 0, 0, 0, c, -m*L*g/2, c];
-
-Btil = [0, 0, 0; L, 0, 0; 0, 0, 0; 0, 1, 0; 0, 0, 0; 0, 0, 1];
-
-A = Minv*Atil;
-
-B = Minv*Btil;
-
-C = eye(6);
-
-D = zeros(6,3);
-
-ee = ss(A,B,C,D); % Espaco de Estados de malha aberta
-
-fts = tf(ee); % Mudanca para FTs
-
-FT_T2_theta2dot = fts(6,3); % FT relacionando thetadot2 x T2
-
-%%% Consertando FT1
-
-[num,den]=tfdata(FT_T2_theta2dot,'v');
-FT_num = [num 0];
-FT_den = [den 0];
-FT_T2_theta2dot = tf(FT_num,FT_den);
-
 %% Calculando num e den ITAE
 
 syms s KP KD KI;
 
 KPID_num = (KD*s^2+KP*s+KI);
 KPID_den = s;
+
+FT_num = 7.281*s^5 + 0.01002*s^4 + 238.1*s^3 + 0.1695*s^2;
+FT_den = s^6 + 0.001017*s^5 + 68.78*s^4 + 0.05918*s^3 + 1168*s^2 + 0.8315*s;
+
 
 Numerador = FT_num * KPID_num;
 expand(Numerador);
@@ -63,3 +24,5 @@ expand(F_den);
 Denominador =F_den + Numerador;
 expand(Denominador);
 collect(Denominador)
+
+coef_den_ITAE = coeffs(collect(Denominador))
